4a9dd98bb39da04f21466166c60c008668216a55
goga <goga@GOGAH-PC-LIN>
2024-06-10 20:21:57 +0300

    ISkCoreGuiConstants.REFDEF_SUPPLIED_SK_CONN_ID is defined

--- org.toxsoft.skf.journals.mws/src/org/toxsoft/skf/journals/e4/uiparts/UipartJournals.java
+++ org.toxsoft.skf.journals.mws/src/org/toxsoft/skf/journals/e4/uiparts/UipartJournals.java
@@ -4,10 +4,7 @@ import org.toxsoft.core.tsgui.bricks.ctx.*;
 import org.toxsoft.core.tsgui.bricks.ctx.impl.*;
 import org.toxsoft.core.tsgui.utils.layout.*;
 import org.toxsoft.core.tsgui.widgets.*;
-import org.toxsoft.core.tslib.utils.logs.impl.*;
 import org.toxsoft.skf.journals.e4.uiparts.main.*;
-import org.toxsoft.uskat.core.connection.*;
-import org.toxsoft.uskat.core.gui.conn.*;
 import org.toxsoft.uskat.core.gui.e4.uiparts.*;
 
 /**
@@ -20,16 +17,6 @@ public class UipartJournals
   // extends MwsAbstractPart {
 
   JournalsPanel panel;
-
-  @Override
-  public ISkConnection skConn() {
-    ISkConnectionSupplier cs = tsContext().get( ISkConnectionSupplier.class );
-    if( cs != null ) {
-      return cs.defConn();
-    }
-    LoggerUtils.errorLogger().error( "ISkConnectionSupplier - null" ); //$NON-NLS-1$
-    return null;
-  }
 
   @Override
   protected void doCreateContent( TsComposite aParent ) {
